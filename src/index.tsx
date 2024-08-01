4946c533eb87e020a420b65b1bc8f19fd8e90341
seung365 <bdh3659@naver.com>
2024-08-01 15:10:29 +0900

    feat: index에서 msw 경로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 async function deferRender() {
   if (process.env.REACT_APP_RUN_MSW === 'true') {
     const { worker } = await import('./mocks/browser');
-    await worker.start();
+    await worker.start({
+      serviceWorker: {
+        url: '/react-deploy/mockServiceWorker.js',
+      },
+    });
+  } else {
   }
 
   return;
